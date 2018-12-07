7776023dbe72e5be3f88adfbfe23f071c6d8c987
cmguyman11 <cmguyman11@gmail.com>
2018-12-06 18:15:37 -0800

    grey

--- enLIGHTen/components/NavBar.js
+++ enLIGHTen/components/NavBar.js
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderWidth: 1,
-    borderColor: "black"
+    borderColor: "grey"
   },
   iconsContainer: {
     display: "flex",
